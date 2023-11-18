834b2f687167191b7adf320325aa0016f6d69c31
elies <elie.stroun@epitech.eu>
2023-11-18 11:11:39 +0100

    map sprite working

--- src/game.c
+++ src/game.c
@@ -10,18 +10,23 @@
 int main() {
     sfVideoMode mode = {1200, 1000, 64};
     sfRenderWindow *window;
-    sfRenderWindow_setFramerateLimit(window, 60);
     sfTexture *mapTexture;
     sfSprite *mapSprite;
+    sfEvent event;
 
     window = sfRenderWindow_create(mode, "Game Window", sfResize | sfClose, NULL);
     if (!window) return -1;
-    mapTexture = sfTexture_createFromFile("/../assets/map_v1.png", NULL);
+
+    mapTexture = sfTexture_createFromFile("assets/map_v1.png", NULL);
+    if (!mapTexture) return -1;
+
     mapSprite = sfSprite_create();
+
     sfSprite_setTexture(mapSprite, mapTexture, sfTrue);
 
+    sfRenderWindow_setFramerateLimit(window, 60);
+
     while (sfRenderWindow_isOpen(window)) {
-        sfEvent event;
         while (sfRenderWindow_pollEvent(window, &event)) {
             if (event.type == sfEvtClosed) {
                 sfRenderWindow_close(window);
