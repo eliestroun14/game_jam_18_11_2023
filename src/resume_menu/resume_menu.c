9fb2a232dac96e72dc1f546309f323c1310b4dd4
elies <elie.stroun@epitech.eu>
2023-11-18 18:04:48 +0100

    a a a

--- src/resume_menu/resume_menu.c
+++ src/resume_menu/resume_menu.c
@@ -5,7 +5,7 @@
 ** resume_menu.c
 */
 
-#include "../include/game_jam.h"
+#include "../../include/game_jam.h"
 #include <SFML/Graphics.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -32,23 +32,27 @@ void resume_menu(sfRenderWindow *window)
     sfVector2f scale = {1.12, 1.12};
     sfVector2f scale_button = {1, 1};
 
-    sfVector2f pos_button = {500, 500};
-    sfVector2f pos_button02 = {500, 650};
-    sfVector2f pos_button03 = {500, 800};
+    sfVector2f pos_button = {400, 300};
+    sfVector2f pos_button02 = {425, 450};
+    sfVector2f pos_button03 = {425, 600};
 
     sfSprite_setTexture(bg_sprite, background, sfTrue);
     sfSprite_setScale(bg_sprite, scale);
 
     while (sfRenderWindow_isOpen(window)) {
-        if (check_event(window, &event) == 1)
+        if (check_event(window, &event) == 1) {
+            sfSprite_destroy(bg_sprite);
+            sfTexture_destroy(background);
             return;
+        }
         sfRenderWindow_clear(window, sfBlack);
         sfRenderWindow_drawSprite(window, bg_sprite, NULL);
         display_resume_button(window, scale_button, pos_button);
         display_restart_button(window, scale_button, pos_button02);
         display_menu_button(window, scale_button, pos_button03);
-        // if_click_play(window, event);
-        // if_click_quit(window, event);
         sfRenderWindow_display(window);
     }
+    sfSprite_destroy(bg_sprite);
+    sfTexture_destroy(background);
+    sfRenderWindow_destroy(window);
 }
